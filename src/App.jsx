4d2e9242bf683ad84d34c88f45234a884f6a3cec
umidjonCoder2009 <umidjonfarxodov140@gmail.com>
2025-03-16 11:52:58 +0500

    umidjon commit project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import Project from './pages/Project'
 
 function App() {
   return (
     <>
     <Routes>
-      
+      <Route path={"/project"} element={<Project/>}/>
     </Routes>
     </>
   )
